4a52749fbf3f4466c29fa171bfffa681fc57e51e
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-07 08:16:51 +0200

    first release

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# fonstr
-fonstr
+
+
+<div align="center">  
+  <h1>fonstr</h1>
+</div>
+
+<div align="center">  
+<i>fonstr</i>
+</div>
+
+---
+
+<div align="center">
+<h4>Documentation</h4>
+</div>
+
+---
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/nostrapps/fonstr/blob/gh-pages/LICENSE)
+[![npm](https://img.shields.io/npm/v/fonstr)](https://npmjs.com/package/fonstr)
+[![npm](https://img.shields.io/npm/dw/fonstr.svg)](https://npmjs.com/package/fonstr)
+[![Github Stars](https://img.shields.io/github/stars/nostrapps/fonstr.svg)](https://github.com/nostrapps/fonstr/)
+
+
+# Fonstr Relay
+
+A simple and efficient Nostr relay server implemented using Fastify and WebSockets.
+
+## Features
+
+- Event filtering based on Nostr specifications
+- Efficient message handling and routing
+- Easy to set up and configure
+
+
+
+### Running the Relay
+
+You can start the relay server using the `fonstr` command provided in the `bin` field of `package.json`. If you have installed the package globally or linked it, you can run:
+
+```bash
+fonstr [port]
+```
+
+Replace `[port]` with the desired port number. If no port is provided, the server will run on port 4444 by default.
+
+If you have not installed the package globally or linked it, you can use `npx` to run the `fonstr` command:
+
+```bash
+npx fonstr [port]
+```
+
+Alternatively, you can run the server using `node`:
+
+```bash
+node index.js [port]
+```
+
+Now the Nostr relay server will be up and running, ready to handle incoming events and subscriptions.
+
+### Installation from source
+
+First, clone the repository and navigate to the project directory:
+
+```bash
+git clone https://github.com/nostrapps/fonstr.git
+cd fonstr
+```
+
+Then, install the necessary dependencies:
+
+```bash
+npm install
+```
+
+
+## API
+
+This Nostr relay server supports the following Nostr event types:
+
+- `EVENT`: Publish an event
+- `REQ`: Subscribe to events with specified filters
+- `CLOSE`: Unsubscribe from a subscription
+- `EOSE`: End of subscription events signal
+
+## Example
+
+```javascript
+// Connect to the Nostr relay server using a WebSocket
+const socket = new WebSocket('ws://localhost:4444');
+
+// Subscribe to events
+socket.send(JSON.stringify(['REQ', 'subscription_id', { types: [1, 2, 3], kind: 1 }]));
+
+// Listen for incoming events
+socket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
+  console.log('Received data:', data);
+});
+```
+
+## License
+
+This project is licensed under the MIT License. See [LICENSE](LICENSE) for more information.
+
+## Contributing
+
+Contributions are welcome! Feel free to open a pull request or report any issues you find.
+
+## License
+
+- MIT
---
+++ index.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+import fastify from 'fastify'
+import fastifyWebsocket from '@fastify/websocket'
+import { validateEvent, verifySignature } from 'nostr-tools'
+
+const port = process.argv[2] || 4444
+
+const events = []
+const subscribers = new Map()
+
+const fi = fastify()
+
+fi.register(fastifyWebsocket)
+fi.register(async function (fastify) {
+  fastify.get('/', { websocket: true }, async (con, req) => {
+    console.log('ws connection started')
+
+    const { socket } = con
+    console.log('ws connection established')
+
+    const eventPassesFilter = (event, filter) => {
+      const { types, kind, from, to } = filter
+
+      if (types && !types.includes(event.type)) {
+        return false
+      }
+
+      if (kind && event.kind !== kind) {
+        return false
+      }
+
+      if (from && event.from !== from) {
+        return false
+      }
+
+      if (to && event.to !== to) {
+        return false
+      }
+
+      return true
+    }
+
+    socket.on('message', async message => {
+      message = message?.toString()
+      console.log('received message', message)
+
+      const [type, value, ...rest] = JSON.parse(message)
+      switch (type) {
+        case 'EVENT':
+          const ok = validateEvent(value)
+          const veryOk = verifySignature(value)
+          console.log('ok', ok, veryOk)
+          if (ok && veryOk) { // Validate the event using nostr-tools
+            events.push(value)
+            console.log('event ok')
+            subscribers.forEach((filters, subscriber) => {
+              filters.forEach(filter => {
+                if (eventPassesFilter(value, filter)) {
+                  subscriber.send(JSON.stringify(['EVENT', filter.subscription_id, value]))
+                }
+              })
+            })
+            socket.send(`["OK", ${value.id}, true, ""]`)
+          } else {
+            socket.send('["NOTICE", "Invalid event"]')
+          }
+          break
+        case 'REQ':
+          console.log('REQ')
+          const subscription_id = value
+          const filters = rest.map(filter => ({ ...filter, subscription_id }))
+          subscribers.set(socket, filters)
+
+          filters.forEach(filter => {
+            events.filter(event => eventPassesFilter(event, filter))
+              .forEach(event => socket.send(JSON.stringify(['EVENT', filter.subscription_id, event])))
+          })
+
+          socket.send(JSON.stringify(['EOSE', subscription_id]))
+          break
+        case 'CLOSE':
+          const sub_id = value
+          if (subscribers.has(socket)) {
+            const updatedFilters = subscribers.get(socket).filter(filter => filter.subscription_id !== sub_id)
+            if (updatedFilters.length === 0) {
+              subscribers.delete(socket)
+            } else {
+              subscribers.set(socket, updatedFilters)
+            }
+          }
+          break
+        default:
+          socket.send('["NOTICE", "Unrecognized event"]')
+          console.log('Unrecognized event')
+        // ...
+      }
+    })
+
+    // ...
+  })
+})
+
+fi.listen({ port }, err => {
+  if (err) throw err
+  console.log(`listening on ${port}`)
+})
